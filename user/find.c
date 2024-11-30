6d03540117d2fd7f4dabc39d4df5ef2e43820f1e
liginity <liginity@outlook.com>
2024-11-30 21:13:23 +0800

    Pass the test for find.c

--- user/find.c
+++ user/find.c
@@ -27,13 +27,9 @@ find(const char *path, const char *name) {
     return;
   }
 
-  if (strcmp(path, name) == 0) {
-    fprintf(1, "%s\n", path);
-  }
-
   // debug
-  printf("line %d\n", __LINE__);
-  printf("path = \"%s\"\n", path);
+  // printf("line %d\n", __LINE__);
+  // printf("path = \"%s\"\n", path);
 
   switch (st.type) {
     case T_DEVICE:
@@ -57,8 +53,12 @@ find(const char *path, const char *name) {
         // skip current dir, or parent dir.
         continue;
       }
+
       memmove(p, de.name, DIRSIZ);
       p[DIRSIZ] = 0;
+      if (strcmp(de.name, name) == 0) {
+        fprintf(1, "%s\n", buffer);
+      }
 
       find(buffer, name);
     }
@@ -78,7 +78,13 @@ main(int argc, char *argv[])
     exit(1);
   }
 
-  find(argv[1], argv[2]);
+  const char *path = argv[1];
+  const char *name = argv[2];
+  if (strcmp(path, name) == 0) {
+    fprintf(1, "%s\n", path);
+  }
+
+  find(path, name);
 
   exit(0);
 }
