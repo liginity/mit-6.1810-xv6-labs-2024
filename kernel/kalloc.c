40d2f5d4453bb0fca7196340acae2cc3d8c729e6
liginity <liginity@outlook.com>
2024-12-10 15:57:06 +0800

    WIP Initial idea about cow page reference count
    I spent quite some time thinking about the interface and implementation
    of the reference count.

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,6 +9,9 @@
 #include "riscv.h"
 #include "defs.h"
 
+#define PA_TO_RC_ARRAY_INDEX(pa) ((PGROUNDDOWN(pa) - KERNBASE) / PGSIZE)
+// #define PA_TO_RC_ARRAY_INDEX(pa) ((PGROUNDDOWN(pa) - KERNBASE) << 12)
+
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
@@ -21,6 +24,8 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
+  // reference count array for physical pages.
+  int phpgrcs[(PHYSTOP - KERNBASE) / PGSIZE];
 } kmem;
 
 void
@@ -35,8 +40,10 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE) {
+    kmem.phpgrcs[PA_TO_RC_ARRAY_INDEX((uint64)p)] = 1;
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by pa,
@@ -50,6 +57,22 @@ kfree(void *pa)
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
+
+  // for physical page reference count
+  if (kmem.phpgrcs[PA_TO_RC_ARRAY_INDEX((uint64)pa)] <= 0) {
+    panic("kfree: reference count error");
+  }
+  // use the mem lock
+  acquire(&kmem.lock);
+  kmem.phpgrcs[PA_TO_RC_ARRAY_INDEX((uint64)pa)] -= 1;
+  int rc = kmem.phpgrcs[PA_TO_RC_ARRAY_INDEX((uint64)pa)];
+  release(&kmem.lock);
+
+  if (rc > 0) {
+    // this page has reference count > 0. it should not be freed.
+    return;
+  }
+  // assert(kmem.phpgrcs[PA_TO_RC_ARRAY_INDEX((uint64)pa)] == 0);
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
@@ -74,6 +97,8 @@ kalloc(void)
   r = kmem.freelist;
   if(r)
     kmem.freelist = r->next;
+  // initialize the reference count for this page.
+  kmem.phpgrcs[PA_TO_RC_ARRAY_INDEX((uint64)r)] = 1;
   release(&kmem.lock);
 
   if(r)
